2fa1c212a17a1adb6d40526b56b995dc0c3ec881
Amelia Sommer <ameliasommer@gmail.com>
2025-03-05 22:11:18 +0000

    Issue #4 - Removing unnecessary imports from UpdateWordRequest

--- src/main/java/com/japanese/study_app/request/UpdateWordRequest.java
+++ src/main/java/com/japanese/study_app/request/UpdateWordRequest.java
@@ -1,13 +1,11 @@
 package com.japanese.study_app.request;
 
-import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
 
 import com.japanese.study_app.model.Category;
 import com.japanese.study_app.model.EnglishWord;
-import com.japanese.study_app.model.ExampleSentence;
-// import com.japanese.study_app.model.WordDefinition;
+
 
 import lombok.Data;
 
