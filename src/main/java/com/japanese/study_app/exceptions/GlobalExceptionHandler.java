c6d4484aefe98015f7fb7f2797da5ece907250ee
Amelia Sommer <ameliasommer@gmail.com>
2025-06-05 22:03:52 +0100

    STUDY-08 | Adding simple GlobalExceptionHandler class

---
+++ src/main/java/com/japanese/study_app/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.japanese.study_app.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(AlreadyExistsException.class)
+    public void alreadyExistsExceptionHandler() {
+
+    }
+}
