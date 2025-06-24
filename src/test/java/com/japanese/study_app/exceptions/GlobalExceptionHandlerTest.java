dc4b0b201d6d368c2281b25493cfec637f93d691
Amelia Sommer <ameliasommer@gmail.com>
2025-06-24 21:19:08 +0100

    STUDY-08 | Adding skeleton GlobalExceptionHandlerTest

---
+++ src/test/java/com/japanese/study_app/exceptions/GlobalExceptionHandlerTest.java
@@ -0,0 +1,38 @@
+package com.japanese.study_app.exceptions;
+
+import org.junit.jupiter.api.Test;
+
+class GlobalExceptionHandlerTest {
+
+    @Test
+    void alreadyExistsExceptionHandler() {
+    }
+
+    @Test
+    void categoryNotFoundException() {
+    }
+
+    @Test
+    void exampleSentenceNotFoundException() {
+    }
+
+    @Test
+    void wordNotFoundException() {
+    }
+
+    @Test
+    void missingExampleSentenceException() {
+    }
+
+    @Test
+    void randomWordRetrievalException() {
+    }
+
+    @Test
+    void ioException() {
+    }
+
+    @Test
+    void normalException() {
+    }
+}
